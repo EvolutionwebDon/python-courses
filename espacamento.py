dcf1a7775a86c6be14fc9cbfc09a71c62dc3b6a3
EvolutionwebDon <45299292+EvolutionwebDon@users.noreply.github.com>
2019-01-10 13:02:41 -0200

    Add files via upload

---
+++ espacamento.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+         #==============python aulas================
+         #==============evolutionwebdon=============
+         #==========================================
+
+num1 = int(input("digite um numero de 1 a 50 :"))
+
+if(num1 > 10):
+    print("valor maior que 10")
+    if(num1<=15):
+        print("maior que 10 mais menor que 15")
+    else:
+        if(num1<=50):
+            print("maior que 15 mais e menor doque 50")
+        else:
+            print("o valor e maior doque 50")
+else:
+    if(num1>5):
+        print("o numero e igual a 10  e maior que 5")
+        if(num1== 7):
+            print("o numero e 7")
+    else:
+        print("o valor e = ou menor que 5.")
+
